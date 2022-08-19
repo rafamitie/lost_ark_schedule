32a110693862042974138c4b07b076f4461f5e3c
rafamitie <69724532+rafamitie@users.noreply.github.com>
2022-08-19 18:07:07 -0300

    deploy heroku

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const porta = process.env.PORT || 8080
 
-app.use(express.static(__dirname+ '/dist/'))
+//
+app.use(express.static(__dirname + '/dist/'))
 
-app.get('/', function(req,res){
-    res.sendfile(__dirname+ '/dist/index.html')
+//responser a requisições feitas para o servidor web por meio da rota get
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/dist/index.html')
 })
 
-app.listen(porta, function(){
-    console.log('ta rodando')
+app.listen(porta, function() { 
+    console.log('Servidor web rodando com Express')
 })
